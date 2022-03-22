a66f18f3b640c5b58672d4f8bc5efd043f4cf91b
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-03-22 23:48:06 +0100

    chore: revert to the most stable solution ... with the added power of cookies

--- background.js
+++ background.js
@@ -1,35 +1,45 @@
 const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
+let imgTabId;
+
 browser.menus.create({
     id: extname,
     title: "Print Image",
     documentUrlPatterns: ["<all_urls>"],
     contexts: ["image"],
-    onclick: (info,tab) => {
-        browser.tabs.executeScript({
-            code:`
-                (function (){
-                    let htmlElement = document.getElementsByTagName("html")[0];
-
-                    const oldHTML = htmlElement.innerHTML;
-
-                    htmlElement.innerHTML = '<head/><body><img id="printimg" /></body>';
-
-                    let img = document.getElementById('printimg');
-
-                    img.onerror = function() {
-                        htmlElement.innerHTML = oldHTML;
-                        alert('Error Loading Image');
-                    }
-                    img.onload = function(){
-                        window.print();
-                        htmlElement.innerHTML = oldHTML;
-                    }
-                    img.src = "${info.srcUrl}";
-                }());
-            `
+    onclick: async (info,tab) => {
+        const ntab = await browser.tabs.create({
+            url: info.srcUrl,
+            active: true,
+            cookieStoreId: tab.cookieStoreId
         });
+        imgTabId = ntab.id;
     }
 });
 
+function handleUpdated(tabId, changeInfo, tabInfo){
+    if(changeInfo.status === 'complete'){
+        if(tabId === imgTabId) {
+            imgTabId = null;
+            // print & close
+            browser.tabs.executeScript({
+                code: `(function(){window.print();window.close();}())`
+            });
+        }
+    }
+}
+
+const filter = {
+  properties: ["status"]
+}
+
+// cleanup
+function handleRemoved(tabId, removeInfo){
+    if(tabId === imgTabId){
+        imgTabId = null;
+    }
+}
+
+browser.tabs.onUpdated.addListener(handleUpdated, filter);
+browser.tabs.onRemoved.addListener(handleRemoved);
